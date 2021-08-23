b6a315dd92745b5efc0e223cfd85045e770ea402
GiverPlay007 <play.giver.007@gmail.com>
2021-08-22 21:49:25 -0300

    Created TagManager abstraction

--- src/main/java/net/moonlar/playertags/PlayerTags.java
+++ src/main/java/net/moonlar/playertags/PlayerTags.java
@@ -1,16 +1,27 @@
 package net.moonlar.playertags;
 
+import net.moonlar.playertags.managers.TagManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PlayerTags extends JavaPlugin {
 
+  private TagManager tagManager;
+
   @Override
   public void onEnable() {
+    saveDefaultConfig();
+    reloadConfig();
 
+    tagManager = new TagManager(this);
+    tagManager.reload();
   }
 
   @Override
   public void onDisable() {
+    tagManager = null;
+  }
 
+  public TagManager getTagManager() {
+    return tagManager;
   }
 }
---
+++ src/main/java/net/moonlar/playertags/managers/TagManager.java
@@ -0,0 +1,28 @@
+package net.moonlar.playertags.managers;
+
+import net.moonlar.playertags.PlayerTags;
+import org.bukkit.entity.Player;
+
+public class TagManager {
+  private final PlayerTags plugin;
+
+  public TagManager(PlayerTags plugin) {
+    this.plugin = plugin;
+  }
+
+  public void start() {
+
+  }
+
+  public void reload() {
+
+  }
+
+  public void update(Player player) {
+
+  }
+
+  public void clear(Player player) {
+
+  }
+}
