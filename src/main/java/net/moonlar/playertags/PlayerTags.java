8e8d96651f9ce494f43517ff1033ee07817cbd02
GiverPlay007 <play.giver.007@gmail.com>
2021-08-22 21:31:49 -0300

    Initialize project

---
+++ src/main/java/net/moonlar/playertags/PlayerTags.java
@@ -0,0 +1,16 @@
+package net.moonlar.playertags;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlayerTags extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+
+  }
+
+  @Override
+  public void onDisable() {
+
+  }
+}
