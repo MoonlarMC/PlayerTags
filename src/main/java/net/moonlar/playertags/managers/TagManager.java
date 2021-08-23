f43bf90efb511e758209b92304ffc7d2820a72c2
GiverPlay007 <play.giver.007@gmail.com>
2021-08-22 22:12:48 -0300

    Partial TagManager implementation

--- src/main/java/net/moonlar/playertags/managers/TagManager.java
+++ src/main/java/net/moonlar/playertags/managers/TagManager.java
@@ -2,24 +2,42 @@ package net.moonlar.playertags.managers;
 
 import net.moonlar.playertags.PlayerTags;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitTask;
 
 public class TagManager {
   private final PlayerTags plugin;
+  private BukkitTask task;
 
   public TagManager(PlayerTags plugin) {
     this.plugin = plugin;
   }
 
-  public void start() {
-
+  public void reload() {
+    stop();
+    start();
   }
 
-  public void reload() {
+  public void start() {
+    task = plugin.getScheduler().repeat(this::updateAll, 100);
+  }
 
+  private void stop() {
+    if(task != null) {
+      task.cancel();
+      task = null;
+    }
+  }
+
+  public void updateAll() {
+    plugin.getServer().getOnlinePlayers().forEach(this::update);
   }
 
   public void update(Player player) {
 
+  }
+
+  public void clearAll() {
+    plugin.getServer().getOnlinePlayers().forEach(this::clear);
   }
 
   public void clear(Player player) {
