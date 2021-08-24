781ff21885d6d518749360a141a69f9d12073223
GiverPlay007 <play.giver.007@gmail.com>
2021-08-23 21:03:47 -0300

    Hook Vault Permission

--- src/main/java/net/moonlar/playertags/PlayerTags.java
+++ src/main/java/net/moonlar/playertags/PlayerTags.java
@@ -1,16 +1,26 @@
 package net.moonlar.playertags;
 
+import net.milkbowl.vault.permission.Permission;
 import net.moonlar.playertags.managers.TagManager;
 import net.moonlar.playertags.utils.Scheduler;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PlayerTags extends JavaPlugin {
 
   private TagManager tagManager;
   private Scheduler scheduler;
+  private Permission permission;
 
   @Override
   public void onEnable() {
+    if(!loadVault()) {
+      getLogger().severe("Could not hook Vault Permission, disabling.");
+      getServer().getPluginManager().disablePlugin(this);
+      return;
+    }
+
     saveDefaultConfig();
     reloadConfig();
 
@@ -23,6 +33,27 @@ public final class PlayerTags extends JavaPlugin {
   public void onDisable() {
     tagManager = null;
     scheduler = null;
+    permission = null;
+  }
+
+  private boolean loadVault() {
+    Plugin vault = getServer().getPluginManager().getPlugin("Vault");
+
+    if(vault == null || !getServer().getPluginManager().isPluginEnabled(vault)) {
+      getLogger().severe("Vault isn't enabled.");
+      return false;
+    }
+
+    RegisteredServiceProvider<Permission> provider =
+      getServer().getServicesManager().getRegistration(Permission.class);
+
+    if(provider == null) {
+      getLogger().severe("No Vault Permission provider registered.");
+      return false;
+    }
+
+    permission = provider.getProvider();
+    return permission != null;
   }
 
   public TagManager getTagManager() {
@@ -31,5 +62,9 @@ public final class PlayerTags extends JavaPlugin {
 
   public Scheduler getScheduler() {
     return scheduler;
+  }
+
+  public Permission getVaultPermission() {
+    return permission;
   }
 }
